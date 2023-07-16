f45531a203e6d3b2d43f26c1438e564a58ffeba6
Roman Shvagla <121064413+immortalua96@users.noreply.github.com>
2023-07-16 13:13:48 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import Notiflix from 'notiflix';
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  
+
+  handleSubmit = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    const isIncludeName = this.state.contacts.some(
+      value => value.name.toLowerCase() === name.toLowerCase()
+    );
+    const isIncludeNumber = this.state.contacts.some(
+      value => value.number.split('-').join('') === number.split('-').join('')
+    );
+
+    if (isIncludeName) {
+     Notiflix.Report.warning(`${name} is already in contacts`);
+      return;
+    }
+    if (isIncludeNumber) {
+      Notiflix.Report.warning(`phonenumber${number} is already in contacts`);
+      
+      return;
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  handleDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  getFilteredContacts = () => {
+    const filterContactsList = this.state.contacts.filter(contact => {
+      return contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toLowerCase());
+    });
+
+    return filterContactsList;
+  };
+
+  render() {
+    const { filter } = this.state;
+
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 20,
+          color: '#010101',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm handleSubmit={this.handleSubmit} />
+        <h2> Contacts</h2>
+        <Filter filter={filter} handleChange={this.handleChange} />
+        <ContactList
+          contacts={this.getFilteredContacts()}
+          handleDelete={this.handleDelete}
+        />
+      </div>
+    );
+  }
+}
